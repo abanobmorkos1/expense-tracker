3b477d8367db7060cafda4eb26ef57f8ba65df49
abanobmorkos1 <abanobmorkos11@gmail.com>
2026-02-08 11:23:04 -0500

    Add MongoDB and Mongoose dependencies; refactor app.js for MongoDB connection

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const app = express();
-const morgan = require('morgan');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const uri = process.env.mongodb_uri;
 
-//  middleware
+// middleware
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Connect to MongoDB
+mongoose.connect(uri);
+
+mongoose.connection
+  .on("open", () => console.log('✅ Connected to MongoDB'))
+  .on("close", () => console.log('❌ Disconnected from MongoDB'))
+  .on("error", (err) => console.log('⚠️ MongoDB Error:', err));
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
